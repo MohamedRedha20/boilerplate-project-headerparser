5747ef2ff65143c873382f96a3e32b96577bb8ec
MohamedRedha20 <rddz5420@gmail.com>
2026-07-17 17:24:42 +0100

    implement header reading

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// whoami
+
+app.get("/api/whoami",(req,res) =>{
+  let ip = req.headers["host"];
+  ip = ip.split(":")[0];
+  let acceptedLangs = req.headers["accept-language"];
+  let software = req.headers["user-agent"];
+  return res.json({
+    "ipaddress" : ip, 
+    "language" : acceptedLangs, 
+    "software" : software})
+})
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
